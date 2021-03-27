4866d7fb38c4cd5901c8d790bcd01bd8af68ce68
旭 杨 <27681135@qq.com>
2021-03-27 21:18:30 +0800

    添加https判断

--- src/boot/axios.ts
+++ src/boot/axios.ts
@@ -6,8 +6,18 @@ declare module '@vue/runtime-core' {
     $axios: AxiosInstance;
   }
 }
+const ishttps = 'https:' === document.location.protocol;
+let base = ''
+if (ishttps) {
+  base = 'https://18.176.191.82:3007/noauth/filoli/'
+} else if (process.env.PROD) {
+  base = 'http://18.176.191.82:3006/noauth/filoli/'
+} else {
+  base = 'http://localhost:3006/noauth/filoli/'
+}
 
-const api = axios.create({ baseURL: process.env.PROD ? 'http://18.176.191.82:3006/noauth/filoli/' : 'http://localhost:3006/noauth/filoli/' });
+
+const api = axios.create({ baseURL: base });
 
 export default boot(({ app }) => {
   // for use inside Vue files (Options API) through this.$axios and this.$api
