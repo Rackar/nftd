5b73821a1bda6d5cd0b63055e6391651ed4528cd
Rackar <27681135@qq.com>
2021-06-03 10:33:17 +0800

    add ABI contract

---
+++ src/web3/contract/LOLI_ABI.js
@@ -0,0 +1,123 @@
+const address_LOLI = "0x9F8F240C3b85870B1447FaC3a554d9391979D267"
+const ABI_LOLI=[
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "dNFTad",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "LOLIad",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "setddNid",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "accountToFetched",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "ddNid",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "fetchLOLI",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "account",
+				"type": "address"
+			}
+		],
+		"name": "getLOLICanClaimOf",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "canClaim",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getLOLIad",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getdNFTad",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "tLtD",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
+
+export {address_LOLI,ABI_LOLI}
---
+++ src/web3/contract/index.js
@@ -0,0 +1,5 @@
+import { address_DNFT,ABI_DNFT } from "./DNFT_ABI";
+import { address_NFT,ABI_NFT } from "./NFT_ABI";
+import { address_LOLI,ABI_LOLI } from "./LOLI_ABI";
+
+export {address_DNFT,ABI_DNFT,address_NFT,ABI_NFT,address_LOLI,ABI_LOLI}
